7b404cc1d2e2b26a1602a6c204f00ff9450fe4d8
root <root@centos-test.obuh.akma.local>
2019-10-09 16:36:04 +0300

    homework

--- 16.log/log.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-yum install -y nano rsyslog
-yes | cp -f /vagrant/conf/client/rsyslog.cong /etc/
-systemctl restart rsyslog.service
--- 16.log/web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum install -y epel-release nginx nano rsyslog
-yes | cp -f /vagrant/conf/client/nginx.conf /etc/nginx
-yes | cp -f /vagrant/conf/client/rsyslog.cong /etc/
-cp -f /vagrant/conf/client/audit.conf /etc/rsyslog.d/
-cp -f /vagrant/conf/client/nginx_error.conf /etc/rsyslog.d/
-systemctl restart nginx.service && systemctl restart rsyslog.service
